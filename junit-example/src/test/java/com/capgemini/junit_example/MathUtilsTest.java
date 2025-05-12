b68fdb09817b817b8af15064b05aee47045ea99b
Akshay Kudalkar <akshaykudalkar.12@gmail.com>
2025-05-12 16:05:57 +0530

    made change in main file

--- junit-example/src/test/java/com/capgemini/junit_example/MathUtilsTest.java
+++ junit-example/src/test/java/com/capgemini/junit_example/MathUtilsTest.java
@@ -14,6 +14,9 @@ import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 @DisplayName("MathUtils Test Suite")
 public class MathUtilsTest {
